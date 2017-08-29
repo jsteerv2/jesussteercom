d97b8f72b73867010c6f9552f698a5a57a5eebc7
Jesus Steer <jsteerv@gmail.com>
2017-08-29 15:06:24 -0500

    inicio

---
+++ data/SiteConfig.js
@@ -0,0 +1,58 @@
+module.exports = {
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  siteTitle: "Jesus Steer", // Site title.
+  siteTitleAlt: "Blog de Jesus Steer | JesusSteer.com", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://jesussteer.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteDescription: "Jesus Steer es un desarrollador web full-stack con experiencia en la creación de sitios web rápidos y optimizados para motores de búsqueda.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
+  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
+  disqusShortname: "jesussteer", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "Jesus Steer", // Username to display in the author segment.
+  userTwitter: "jsteerv", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Cartagena, Colombia", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription: "Desarrollador web full-stack con enfoque en la creación de sitios web veloces y optimizados para SEO.",
+
+  userLinks: [
+    {
+      label: "Facebook",
+      url: "https://www.facebook.com/JesusSteer",
+      iconClassName: "fa fa-facebook"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/jsteerv",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Google+",
+      url: "https://plus.google.com/+JesusSteerV",
+      iconClassName: "fa fa-google-plus"
+    },
+    {
+      label: "Youtube",
+      url: "https://www.youtube.com/channel/UCCSk-U--hzLAytT3HtChFnA",
+      iconClassName: "fa fa-youtube"
+    },
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/jesteer/",
+      iconClassName: "fa fa-linkedin"
+    },
+    {
+      label: "Github",
+      url: "https://github.com/jsteerv",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Email",
+      url: "mailto:jsteerv@gmail.com",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "Copyright © 2017. JesusSteer.com"
+};
---
+++ src/components/ToolbarTitle/ToolbarTitle.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Link from "gatsby-link";
+import config from "../../../data/SiteConfig";
+import "./ToolbarTitle.scss";
+
+export default class ToolbarTitle extends Component {
+  render() {
+    return (
+      <Link to="/" className="toolbar-title">
+        <h2 className="toolbar-title-text">
+          {config.siteTitle}
+        </h2>
+        <img
+          src="/logos/logo-64.png"
+          alt="Website Logo"
+          className="toolbar-avatar"
+        />
+      </Link>
+    );
+  }
+}
